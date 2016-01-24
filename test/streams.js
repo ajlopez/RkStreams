572b447b9a5886f803870532a8c08a7bba081ddc
ajlopez <webmaster@ajlopez.com>
2016-01-24 11:50:17 -0300

    additional test, post three messages to stream

--- test/streams.js
+++ test/streams.js
@@ -14,3 +14,25 @@ exports['create and use stream'] = function (test) {
     
     stream.post(1);
 };
+
+exports['post three messages to stream'] = function (test) {
+    test.async();
+    
+    var counter = 0;
+    
+    var stream = rks.stream();
+    
+    stream.process(function (data) {
+        counter++;
+        
+        test.ok(data);
+        test.equal(data, counter);
+        
+        if (data === 3)
+            test.done();
+    });
+    
+    stream.post(1);
+    stream.post(2);
+    stream.post(3);
+};
