5e3656d37666713e5065bfa9b276555ff61fc6cc
ajlopez <webmaster@ajlopez.com>
2016-01-24 11:47:55 -0300

    create and use stream

---
+++ test/streams.js
@@ -0,0 +1,16 @@
+
+var rks = require('..');
+
+exports['create and use stream'] = function (test) {
+    test.async();
+    
+    var stream = rks.stream();
+    
+    stream.process(function (data) {
+        test.ok(data);
+        test.equal(data, 1);
+        test.done();
+    });
+    
+    stream.post(1);
+};
