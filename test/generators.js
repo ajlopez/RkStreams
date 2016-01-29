6252f0170e5cc9396d81716839e2703fe025a7b4
ajlopez <webmaster@ajlopez.com>
2016-01-29 07:23:10 -0300

    first generator using function

---
+++ test/generators.js
@@ -0,0 +1,25 @@
+
+var rks = require('..');
+
+exports['generator using function'] = function (test) {
+    test.async();
+    
+    var generator = rks.generator(function () { return 1; }, { times: 3, frequency: 100 });
+    var stream = rks.stream();
+    
+    var counter = 0;
+    
+    stream.process(function (data) {
+        counter++;
+        
+        test.equal(data, 1);
+        test.ok(counter <= 3);
+        
+        if (counter === 3)
+            test.done();
+    });
+    
+    generator.post(stream);
+    generator.start();
+};
+
