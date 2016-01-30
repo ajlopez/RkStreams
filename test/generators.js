3ce1ea199745a86d23ceaeea7e45b60c57c8cf6b
ajlopez <webmaster@ajlopez.com>
2016-01-30 13:00:59 -0300

    constant generator

--- test/generators.js
+++ test/generators.js
@@ -23,3 +23,24 @@ exports['generator using function'] = function (test) {
     generator.start();
 };
 
+exports['constant generator'] = function (test) {
+    test.async();
+    
+    var generator = rks.generator(1, { times: 3, frequency: 100 });
+    var stream = rks.stream();
+    
+    var counter = 0;
+    
+    stream.process(function (data) {
+        counter++;
+        
+        test.equal(data, 1);
+        test.ok(counter <= 3);
+        
+        if (counter === 3)
+            test.done();
+    });
+    
+    generator.post(stream);
+    generator.start();
+};
